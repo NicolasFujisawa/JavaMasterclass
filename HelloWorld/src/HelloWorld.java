4aeb33d5b48b2a8d3437484f11b2b3a8fa6ed978
Nicolas Fernando Espinoza Fujisawa <50681635+NicolasFujisawa@users.noreply.github.com>
2020-06-17 14:04:23 -0300

    Fix HelloWorld and Primitive data types files.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,27 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		/*
-		 * byte
-		 * char 
-		 * short
-		 * int
-		 * long
-		 * float
-		 * double
-		 * 
-		 * */
-		
-		char myChar = 'D';
-		
-		char myUnicode = '\u00A9';
-		String myString = "Copyright" + myUnicode;
-		
-		System.out.println("My char: " + myChar);
-		System.out.println("My Unicode: " + myUnicode);
-		System.out.println("My Copyright: " + myString);
-		
-		
+		System.out.println("Hello World");
 	}
 
 }
--- PrimitiveDataTypes/src/Main.java
+++ PrimitiveDataTypes/src/Main.java
@@ -2,7 +2,27 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * byte
+		 * char 
+		 * short
+		 * int
+		 * long
+		 * float
+		 * double
+		 * 
+		 * */
+		
+		char myChar = 'D';
+		
+		char myUnicode = '\u00A9';
+		String myString = "Copyright" + myUnicode;
+		
+		System.out.println("My char: " + myChar);
+		System.out.println("My Unicode: " + myUnicode);
+		System.out.println("My Copyright: " + myString);
+		
 
 	}
 
