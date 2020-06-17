fa05b76886b4209e82662630c6fd85b5e6a2611f
Nicolas Fernando Espinoza Fujisawa <50681635+NicolasFujisawa@users.noreply.github.com>
2020-06-17 12:37:17 -0300

    Hello World implementations and Primitives data types.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,15 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		char myChar = 'D';
+		
+		char myUnicode = '\u00A9';
+		String myString = "Copyright" + myUnicode;
+		
+		System.out.println("My char: " + myChar);
+		System.out.println("My Unicode: " + myUnicode);
+		System.out.println("My Copyright: " + myString);
 	}
 
 }
